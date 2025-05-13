52f89cc05ca9f28a2ba879f2a0a0614139cb0627
Christian Gabrisch <github@cgabrisch.de>
2025-05-13 22:08:02 +0200

    Formatted by Spotless

--- src/main/java/de/cgabrisch/metering/Record.java
+++ src/main/java/de/cgabrisch/metering/Record.java
@@ -1,8 +1,5 @@
 package de.cgabrisch.metering;
 
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,16 +7,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 @Entity
 public class Record {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long recordId;
 
   @ManyToOne
-  @JoinColumn(name="meter_id")
+  @JoinColumn(name = "meter_id")
   private Meter meter;
 
   @Column(name = "ts")
