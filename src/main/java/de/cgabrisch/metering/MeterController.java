1f3fb351d6ba6c7fa59802beeda4c72bb5d9a90e
Christian Gabrisch <github@cgabrisch.de>
2025-05-15 22:38:42 +0200

    Encapsulate domain methods in services

--- src/main/java/de/cgabrisch/metering/MeterController.java
+++ src/main/java/de/cgabrisch/metering/MeterController.java
@@ -10,53 +10,33 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MeterController {
-  private final MeterRepository meterRepository;
-  private final RecordRepository recordRepository;
+  private final MeterService meterService;
+    private final RecordService recordService;
 
-  MeterController(MeterRepository meterRepository, RecordRepository recordRepository) {
-    this.meterRepository = meterRepository;
-    this.recordRepository = recordRepository;
+  MeterController(MeterService meterService, RecordService recordService) {
+      this.meterService = meterService;
+      this.recordService = recordService;
   }
 
   @QueryMapping
   public List<Meter> meters() {
-    return meterRepository.findAll();
+    return meterService.findAll();
   }
 
   @MutationMapping
   public Meter createMeter(
       @Argument String serialNumber, @Argument String unit, @Argument String description) {
-    Meter meter = new Meter();
-    meter.setSerialNumber(serialNumber);
-    meter.setUnit(unit);
-    meter.setDescription(description);
-
-    meter = meterRepository.save(meter);
-    return meter;
+    return meterService.createMeter(serialNumber, unit, description);
   }
 
   @QueryMapping
   public List<Record> records(@Argument String serialNumber) {
-    return recordRepository.findBySerialNumber(serialNumber);
+    return recordService.getRecordsForMeter(serialNumber);
   }
 
   @MutationMapping
   public Record addRecord(
       @Argument String serialNumber, @Argument ZonedDateTime timestamp, @Argument BigDecimal read) {
-    Meter meter =
-        meterRepository
-            .findBySerialNumber(serialNumber)
-            .orElseThrow(
-                () ->
-                    new IllegalArgumentException(
-                        String.format("No meter with serial number '%s'", serialNumber)));
-    Record r = new Record();
-    r.setMeter(meter);
-    r.setTimestamp(timestamp);
-    r.setRead(read);
-
-    r = recordRepository.save(r);
-
-    return r;
+    return recordService.addRecordToMeter(serialNumber, timestamp, read);
   }
 }
---
+++ src/main/java/de/cgabrisch/metering/MeterService.java
@@ -0,0 +1,36 @@
+package de.cgabrisch.metering;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class MeterService {
+    private final MeterRepository meterRepository;
+
+    public MeterService(MeterRepository meterRepository) {
+        this.meterRepository = meterRepository;
+    }
+
+    public List<Meter> findAll() {
+        return meterRepository.findAll();
+    }
+
+    public Meter createMeter(
+            String serialNumber, String unit, String description) {
+        Meter meter = new Meter();
+        meter.setSerialNumber(serialNumber);
+        meter.setUnit(unit);
+        meter.setDescription(description);
+
+        meter = meterRepository.save(meter);
+        return meter;
+    }
+
+    public Optional<Meter> findBySerialNumber(String serialNumber) {
+        return meterRepository.findBySerialNumber(serialNumber);
+    }
+}
