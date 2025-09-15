39e9792853095424da50f56e0ed34b747248615e
Christian Gabrisch <mail@cgabrisch.de>
2025-09-15 15:50:36 +0200

    Test findBySerialNumber

---
+++ backend/src/test/java/de/cgabrisch/metering/domain/MeterRepositoryIT.java
@@ -0,0 +1,35 @@
+package de.cgabrisch.metering.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ActiveProfiles("integration-test")
+@Transactional
+class MeterRepositoryIT {
+  @Autowired private EntityManager entityManager;
+
+  @Autowired private MeterRepository meterRepository;
+
+  @Test
+  void testFindBySerialNumber() {
+    Meter meter = new Meter();
+    meter.setSerialNumber("123456");
+    meter.setUnit("kWh");
+    meter.setDescription("Electricity meter");
+
+    entityManager.persist(meter);
+
+    assertTrue(meterRepository.findBySerialNumber("123456").isPresent());
+    assertEquals(
+        meter.getMeterId(), meterRepository.findBySerialNumber("123456").get().getMeterId());
+
+    assertTrue(meterRepository.findBySerialNumber("123457").isEmpty());
+  }
+}
